8812adf25f10a7cf5713e1b7ee060833816d3c84
Henrique Oliveira <henriqueoliveira101@hotmail.com>
2019-04-17 09:23:09 -0400

    Added heroku deployed link to apiConfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://snapback-to-reality.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
